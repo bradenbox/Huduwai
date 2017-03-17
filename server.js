7e6ab3cb10ae6d512a0fef03bca0594d3f41ec80
Deepika Agarwal <dagarwal@halogensoftware.com>
2017-03-17 12:43:01 -0400

    check for keyword

--- server.js
+++ server.js
@@ -60,15 +60,15 @@ slapp.message('(.*)', ['direct_mention'], (msg, text, match2) => {
 
 slapp.route('handleKnows', (msg, state2) =>{
 	var listOfAllowedNames = ['java','programming','html','software','development','testing'];
-	msg.say(state2.what);
-	/*if(listOfAllowedNames.indexOf(state.what) > -1)
+	//msg.say(state2.what);
+	if(listOfAllowedNames.indexOf(state.what) > -1)
 	{
 		msg.say(sendRequestForRecommendation());
 	}
 	else
 	{
 		msg.say("Sorry!");
-	}*/
+	}
 }) 
 
 function sendRequestForRecommendation(){
